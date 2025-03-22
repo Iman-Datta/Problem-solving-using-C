84ad420439cfbd89be55c255823a2a9954a84665
Iman Datta <dattaiman56@gmail.com>
2025-03-22 20:55:33 +0530

    prime number

---
+++ Sir_C/prime_num.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <math.h>
+
+int main() {
+    int num, status = 0;
+    printf("Enter your number:");
+    scanf("%d", &num);
+    if (num <= 1) {
+        printf("0 is not a prime number\n");
+    }
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            status = 1;
+            break;
+        }
+        else if (num % i != 0) {
+            status = 0;
+            break;
+        }
+    }
+    if (status == 1) {
+        printf("%d is a composite number\n", num);
+    }
+    else{
+        printf("%d is a prime number \n", num);
+    }
+    return 0;
+}
